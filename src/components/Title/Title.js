35e985db1c89f65d2aa257e2dbce4faa1e1c678a
SatyamVyas <vyas.satyam.04@gmail.com>
2023-12-25 12:29:33 +0530

    Changed Title Alignment

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,48 +1,90 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import './Title.scss';
+import React, { useState, useEffect, useCallback } from "react";
+import "./Title.scss";
 
+const fonts = [
+	`'Cloister Black', sans-serif`,
+	`'ARB 85 Poster Script JAN-39', sans-serif`,
+	"Glass Antiqua",
+	"Special Elite",
+	`'Old English Five', sans-serif`,
+	`'Permanent Marker'`,
+	`'USAngel', sans-serif`,
+	`Splash`,
+];
 
-const fonts = [`'Cloister Black', sans-serif`, `'Old English Five', sans-serif`, `'USAngel', sans-serif`, `'ARB 85 Poster Script JAN-39', sans-serif`,'Glass Antiqua','Special Elite',`'Cloister Black', sans-serif`, `'Old English Five', sans-serif`, `'USAngel', sans-serif`,`'Cloister Black', sans-serif`, `'Old English Five', sans-serif`, `'USAngel', sans-serif`, `'ARB 85 Poster Script JAN-39', sans-serif`,'Glass Antiqua','Special Elite',`'Cloister Black', sans-serif`, `'Old English Five', sans-serif`, `'USAngel', sans-serif`];
-const letters = ['S', 'P', 'I', 'T', '-', 'H', 'A', 'C', 'K', 'A', 'T', 'H', 'O', 'N', "'", '2', '4'];
+const letters1 = ["S", "P", "I", "T"];
+const letters2 = ["H", "A", "C", "K", "A", "T", "H", "O", "N"];
+const letters3 = ["2", "0", "2", "4"];
 
 const Title = () => {
-  const getRandomFont = useCallback(() => fonts[Math.floor(Math.random() * fonts.length)], []);
+	const getRandomFont = useCallback(
+		() => fonts[Math.floor(Math.random() * fonts.length)],
+		[]
+	);
 
-  const [letterFonts, setLetterFonts] = useState(() => letters.map(() => getRandomFont()));
+	const [letterFonts, setLetterFonts] = useState(() =>
+		letters2.map(() => getRandomFont())
+	);
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      console.log('Updating fonts');
-      setLetterFonts(() => letters.map(() => getRandomFont()));
-    }, 1000);
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			setLetterFonts(() => letters2.map(() => getRandomFont()));
+		}, 350);
 
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [letterFonts, getRandomFont]);
+		return () => {
+			clearInterval(intervalId);
+		};
+	}, [letterFonts, getRandomFont]);
 
-  return (
-    <>
-    <style>@import url('https://fonts.googleapis.com/css2?family=Glass+Antiqua&family=Permanent+Marker&family=Sevillana&family=Silkscreen&family=Single+Day&family=Special+Elite&family=Splash&display=swap');
-    @import url('https://fonts.cdnfonts.com/css/cloister-black');
-@import url('https://fonts.cdnfonts.com/css/old-english-five');
-@import url('https://fonts.cdnfonts.com/css/usangel');
-@import url('https://fonts.cdnfonts.com/css/arb-85-poster-script-jan-39');</style>
-    <div className="container">
-      <h2 className="loki">
-        {letters.map((letter, index) => (
-          <span
-            key={index}
-            className="letter"
-            style={{ fontFamily: letterFonts[index] }}
-          >
-            {letter}
-          </span>
-        ))}
-      </h2>
-    </div>
-    </>
-  );
+	return (
+		<>
+			<style>
+				@import
+				url('https://fonts.googleapis.com/css2?family=Glass+Antiqua&family=Permanent+Marker&family=Special+Elite&family=Splash&display=swap');
+				@import url('https://fonts.cdnfonts.com/css/cloister-black');
+				@import url('https://fonts.cdnfonts.com/css/old-english-five');
+				@import url('https://fonts.cdnfonts.com/css/usangel'); @import
+				url('https://fonts.cdnfonts.com/css/arb-85-poster-script-jan-39');
+			</style>
+			<div className="container">
+				<h3 className="title spit">
+					{letters1.map((letters1, index) => (
+						<span
+							key={index}
+							className="letter"
+							style={{ fontFamily: letterFonts[index] }}
+						>
+							{letters1}
+						</span>
+					))}
+				</h3>
+				<br></br>
+				<h2 className="title hackathon">
+					{letters2.map((letters2, index) => (
+						<span
+							key={index}
+							className="letter"
+							style={{ fontFamily: letterFonts[index] }}
+						>
+							{letters2}
+						</span>
+					))}
+				</h2>
+				<br></br>
+				<h3 className="title year">
+					{letters3.map((letters3, index) => (
+						<span
+							key={index}
+							className="letter"
+							style={{ fontFamily: letterFonts[index] }}
+						>
+							{letters3}
+						</span>
+					))}
+				</h3>
+			</div>
+		</>
+	);
 };
 
 export default Title;

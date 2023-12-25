bd681f83ec1451bc45e4b3d653f173ac2311cf76
SatyamVyas <vyas.satyam.04@gmail.com>
2023-12-25 14:26:54 +0530

    Button Fixed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import ParticleBackground from "./components/ParticlesBackground";
-import './App.css'
+import "./App.css";
 import Title from "./components/Title/Title";
 import Unstopbtn from "./components/Unstopbtn/Unstopbtn";
 
 function App() {
-  return (
-    <div className="App outercontainer">
-     
-      <div className="particlecontainer">
-        <ParticleBackground/>
-      </div>
-       
-      <div className="imgcontainer"></div>
-      <div className="titlesect" style={{opacity:0.8}}>
-        <Title/>
-        <Unstopbtn/>
-      </div>
-      
-    </div>
-  );
+	return (
+		<div className="App outercontainer">
+			<div className="particlecontainer">
+				<ParticleBackground />
+			</div>
+
+			<div className="imgcontainer"></div>
+			<div className="titlesect" style={{ opacity: 0.8 }}>
+				<Title></Title>
+			</div>
+			<Unstopbtn></Unstopbtn>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Unstopbtn/Unstopbtn.js
+++ src/components/Unstopbtn/Unstopbtn.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import './unstopbtn.css'
+import React from "react";
+import "./unstopbtn.css";
 
 const Unstopbtn = () => {
-  return (
-    <div className='unstopbtn'>
-        VIEW ON UNSTOP
-    </div>
-  )
-}
+	return (
+		<div className="container">
+			<div className="btn">
+				<a href="https://www.google.com">View On UnStop</a>
+			</div>
+		</div>
+	);
+};
 
-export default Unstopbtn
+export default Unstopbtn;

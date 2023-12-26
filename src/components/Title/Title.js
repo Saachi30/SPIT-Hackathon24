3ae40a12302f0bc36a53cf425474522402695fcd
SatyamVyas <vyas.satyam.04@gmail.com>
2023-12-26 16:37:08 +0530

    Resposive | Button Changes | Backgrounds Added

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import "./Title.scss";
 
 const fonts = [
@@ -16,25 +16,30 @@ const letters1 = ["S", "P", "I", "T"];
 const letters2 = ["H", "A", "C", "K", "A", "T", "H", "O", "N"];
 const letters3 = ["2", "0", "2", "4"];
 
-const Title = () => {
-	const getRandomFont = useCallback(
-		() => fonts[Math.floor(Math.random() * fonts.length)],
-		[]
-	);
+const getRandomFont = () => fonts[Math.floor(Math.random() * fonts.length)];
 
-	const [letterFonts, setLetterFonts] = useState(() =>
+const Title = () => {
+	const [letterFonts1, setLetterFonts1] = useState(() =>
+		letters1.map(() => getRandomFont())
+	);
+	const [letterFonts2, setLetterFonts2] = useState(() =>
 		letters2.map(() => getRandomFont())
+	);
+	const [letterFonts3, setLetterFonts3] = useState(() =>
+		letters3.map(() => getRandomFont())
 	);
 
 	useEffect(() => {
 		const intervalId = setInterval(() => {
-			setLetterFonts(() => letters2.map(() => getRandomFont()));
+			setLetterFonts1(() => letters1.map(() => getRandomFont()));
+			setLetterFonts2(() => letters2.map(() => getRandomFont()));
+			setLetterFonts3(() => letters3.map(() => getRandomFont()));
 		}, 350);
 
 		return () => {
 			clearInterval(intervalId);
 		};
-	}, [letterFonts, getRandomFont]);
+	}, []);
 
 	return (
 		<>
@@ -48,37 +53,37 @@ const Title = () => {
 			</style>
 			<div className="container">
 				<h3 className="title spit">
-					{letters1.map((letters1, index) => (
+					{letters1.map((letter, index) => (
 						<span
 							key={index}
 							className="letter"
-							style={{ fontFamily: letterFonts[index] }}
+							style={{ fontFamily: letterFonts1[index] }}
 						>
-							{letters1}
+							{letter}
 						</span>
 					))}
 				</h3>
-				<br></br>
+				<br />
 				<h2 className="title hackathon">
-					{letters2.map((letters2, index) => (
+					{letters2.map((letter, index) => (
 						<span
 							key={index}
 							className="letter"
-							style={{ fontFamily: letterFonts[index] }}
+							style={{ fontFamily: letterFonts2[index] }}
 						>
-							{letters2}
+							{letter}
 						</span>
 					))}
 				</h2>
-				<br></br>
+				<br />
 				<h3 className="title year">
-					{letters3.map((letters3, index) => (
+					{letters3.map((letter, index) => (
 						<span
 							key={index}
 							className="letter"
-							style={{ fontFamily: letterFonts[index] }}
+							style={{ fontFamily: letterFonts3[index] }}
 						>
-							{letters3}
+							{letter}
 						</span>
 					))}
 				</h3>

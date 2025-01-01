bf18e4ec225af309b9b6f1c6dbacd79377fb54fb
Satyam Vyas <vyas.satyam.04@gmail.com>
2025-01-01 13:28:38 +0530

    Update prizes.js

--- src/components/Prizes/prizes.js
+++ src/components/Prizes/prizes.js
@@ -12,23 +12,23 @@ const Prizes = () => {
 		});
 	  }, []);
 	return (
-		<div class="outer">
-			<h2 class="heading">Prizes</h2>
-			<div class="box">
-				<div class="innerbox" data-aos="fade-up">
-					<h3 class="rank" data-aos="zoom-in">Grand Prize Pool</h3>
-					<h2 class="prize" data-aos="zoom-in">₹ 1,00,000+</h2>
-					<p class="extras">
+		<div className="outer">
+			<h2 className="heading">Prizes</h2>
+			<div className="box">
+				<div className="innerbox" data-aos="fade-up">
+					<h3 className="rank" data-aos="zoom-in">Grand Prize Pool</h3>
+					<h2 className="prize" data-aos="zoom-in">₹ 1,00,000+</h2>
+					<p className="extras">
 						With Internship Opportunities, Exciting Goodies & Lots
 						More!
 					</p>
 				</div>
-				<div class="innerbox" data-aos="fade-up">
-					<h3 class="rank" data-aos="zoom-in">Certificates</h3>
-					<h2 class="prize" data-aos="zoom-in">
+				<div className="innerbox" data-aos="fade-up">
+					<h3 className="rank" data-aos="zoom-in">Certificates</h3>
+					<h2 className="prize" data-aos="zoom-in">
 						Certificate of Excellence for Top 3 Teams
 					</h2>
-					<p class="extras">
+					<p className="extras">
 						Certificate of Distinction for Top 10 Teams &
 						Certificate of Participation to All Teams!
 					</p>

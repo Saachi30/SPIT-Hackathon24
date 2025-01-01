ad0085b60bae362e0b1e15119a82635ae688af0b
Satyam Vyas <vyas.satyam.04@gmail.com>
2025-01-01 13:29:03 +0530

    Update Timeline.js

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -12,74 +12,74 @@ const Timeline = () => {
 		});
 	  }, []);
     return (
-        <div className='boxtimeline'>
-            <h3 class="h3">Timeline</h3>
-            <ul class="timeline">
+        <div classNameName='boxtimeline'>
+            <h3 className="h3">Timeline</h3>
+            <ul className="timeline">
             <li>
                     <div data-aos="fade-up">
-                        <div className="flag-wrapper roundbox">
-                            <span class="flag special">Round 1</span>
-                            {/* <span class="time-wrapper"><span class="time">2013 - present</span></span> */}
+                        <div classNameName="flag-wrapper roundbox">
+                            <span className="flag special">Round 1</span>
+                            {/* <span className="time-wrapper"><span className="time">2013 - present</span></span> */}
                         </div>
-                        {/* <div class="desc">Registrations and Submission round start</div> */}
+                        {/* <div className="desc">Registrations and Submission round start</div> */}
                     </div>
                 </li>
                 <li>
-                    <div class="direction-r" data-aos="fade-left">
-                        <div class="flag-wrapper">
-                            <span class="flag">28th December 2023</span>
-                            {/* <span class="time-wrapper"><span class="time">2013 - present</span></span> */}
+                    <div className="direction-r" data-aos="fade-left">
+                        <div className="flag-wrapper">
+                            <span className="flag">28th December 2023</span>
+                            {/* <span className="time-wrapper"><span className="time">2013 - present</span></span> */}
                         </div>
-                        <div class="desc">Registrations and Submission round start</div>
+                        <div className="desc">Registrations and Submission round start</div>
                     </div>
                 </li>
 
                 <li>
-                    <div class="direction-l" data-aos="fade-right">
-                        <div class="flag-wrapper">
-                            <span class="flag">26th January 2024</span>
-                            {/* <span class="time-wrapper"><span class="time">2011 - 2013</span></span> */}
+                    <div className="direction-l" data-aos="fade-right">
+                        <div className="flag-wrapper">
+                            <span className="flag">26th January 2024</span>
+                            {/* <span className="time-wrapper"><span className="time">2011 - 2013</span></span> */}
                         </div>
-                        <div class="desc">Registrations and Submission round ends</div>
+                        <div className="desc">Registrations and Submission round ends</div>
                     </div>
                 </li>
 
 
                 <li>
-                    <div class="direction-r" data-aos="fade-left">
-                        <div class="flag-wrapper">
-                            <span class="flag">31st January 2024</span>
-                            {/* <span class="time-wrapper"><span class="time">2008 - 2011</span></span> */}
+                    <div className="direction-r" data-aos="fade-left">
+                        <div className="flag-wrapper">
+                            <span className="flag">31st January 2024</span>
+                            {/* <span className="time-wrapper"><span className="time">2008 - 2011</span></span> */}
                         </div>
-                        <div class="desc">Shortlisted teams announcement</div>
+                        <div className="desc">Shortlisted teams announcement</div>
                     </div>
                 </li>
                 <li>
                     <div data-aos="fade-up">
-                        <div className="flag-wrapper roundbox">
-                            <span class="flag special">Round 2</span>
-                            {/* <span class="time-wrapper"><span class="time">2013 - present</span></span> */}
+                        <div classNameName="flag-wrapper roundbox">
+                            <span className="flag special">Round 2</span>
+                            {/* <span className="time-wrapper"><span className="time">2013 - present</span></span> */}
                         </div>
-                        {/* <div class="desc">Registrations and Submission round start</div> */}
+                        {/* <div className="desc">Registrations and Submission round start</div> */}
                     </div>
                 </li>
                 <li>
-                    <div class="direction-l" data-aos="fade-right">
-                        <div class="flag-wrapper">
-                            <span class="flag">9th February 2024</span>
-                            {/* <span class="time-wrapper"><span class="time">2008 - 2011</span></span> */}
+                    <div className="direction-l" data-aos="fade-right">
+                        <div className="flag-wrapper">
+                            <span className="flag">9th February 2024</span>
+                            {/* <span className="time-wrapper"><span className="time">2008 - 2011</span></span> */}
                         </div>
-                        <div class="desc">Problem statement preference form release</div>
+                        <div className="desc">Problem statement preference form release</div>
                     </div>
                 </li>
 
                 <li>
-                    <div class="direction-r" data-aos="fade-left">
-                        <div class="flag-wrapper">
-                            <span class="flag">10th-11th February 2024</span>
-                            {/* <span class="time-wrapper"><span class="time">2008 - 2011</span></span> */}
+                    <div className="direction-r" data-aos="fade-left">
+                        <div className="flag-wrapper">
+                            <span className="flag">10th-11th February 2024</span>
+                            {/* <span className="time-wrapper"><span className="time">2008 - 2011</span></span> */}
                         </div>
-                        <div class="desc">Grand Finale at Sardar Patel Institute of Technology</div>
+                        <div className="desc">Grand Finale at Sardar Patel Institute of Technology</div>
                     </div>
                 </li>
 
